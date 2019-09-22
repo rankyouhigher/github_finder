b326b1b041a60d30437159aa4a2cecb1c66ad0c2
shubhamexpo <skd8238@gmail.com>
2019-09-22 15:41:55 +0530

    working on Fragments and JSX

--- src/App.js
+++ src/App.js
@@ -2,11 +2,14 @@ import React, { Component, Fragment } from 'react';
 import './App.css';
 
 class App extends Component {
+  foo = () => 'Bar';
   render() {
-    return React.createElement(
-      'div',
-      { className: 'App' },
-      React.createElement('h1', null, 'Hello React!')
+    let name = 'shubham';
+    return (
+      <Fragment>
+        <h1>Hello {name.toUpperCase()}</h1>
+        {this.foo()}
+      </Fragment>
     );
   }
 }
