1298ca375aa478b4948018f029c939bc8aa52e62
shubhamexpo <skd8238@gmail.com>
2019-09-22 20:56:06 +0530

    Lists & passing state with props

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
-import React, { Component, Fragment } from 'react'
+import React, { Component } from 'react'
 import Navbar from './components/layout/Navbar'
-import UserItem from './components/users/UserItem'
+import Users from './components/users/Users'
 import './App.css'
 
 class App extends Component {
   render() {
     return (
-      <div className='navbar bg-primary'>
+      <div className='App'>
         <Navbar />
-        <UserItem />
+        <div className='container'>
+          <Users />
+        </div>
       </div>
     )
   }
--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -12,7 +12,7 @@ export class Navbar extends Component {
   }
   render() {
     return (
-      <div>
+      <div className='navbar bg-primary'>
         <h1>
           <i className={this.props.icon}></i> {this.props.title}
         </h1>
