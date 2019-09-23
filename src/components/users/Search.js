493a53dea38d55654d17617a60580897b3335940
shubhamexpo <skd8238@gmail.com>
2019-09-23 12:50:26 +0530

    Events and Search Components

---
+++ src/components/users/Search.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react'
+
+class Search extends Component {
+  state = {
+    text: ''
+  }
+  onSubmit = e => {
+    e.preventDefault()
+    console.log(e.state.text)
+  }
+  onChange = e => this.setState({ [e.target.name]: e.target.value })
+  render() {
+    return (
+      <div>
+        <form onSubmit={this.onSubmit} className='form'>
+          <input
+            type='text'
+            name='text'
+            placeholder='Search...'
+            value={this.state.text}
+            onChange={this.onChange}
+          />
+          <input
+            type='submit'
+            value='Search'
+            className='btn btn-dark btn-block'
+          />
+        </form>
+      </div>
+    )
+  }
+}
+
+export default Search
