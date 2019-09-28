be4dc6a1edec86d60acd10de6572f36941702e1a
shubhamexpo <skd8238@gmail.com>
2019-09-28 16:17:01 +0530

    repos and repo item data

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ class App extends Component {
   state = {
     users: [],
     user: {},
+    repos: [],
     loading: false,
     alert: null
   }
@@ -40,6 +41,14 @@ class App extends Component {
     )
     this.setState({ user: res.data, loading: false })
   }
+  //Get Single Github UserRepos
+  getUserRepos = async username => {
+    this.setState({ loading: true })
+    const res = await axios.get(
+      `https://api.github.com/users/${username}/repos?per_page=5&sort=created:asc&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+    )
+    this.setState({ repos: res.data, loading: false })
+  }
 
   claerUsers = () => this.setState({ users: [], loading: false })
 
@@ -49,7 +58,7 @@ class App extends Component {
   }
 
   render() {
-    const { users, user, loading } = this.state
+    const { users, user, repos, loading } = this.state
     return (
       <Router>
         <div className='App'>
@@ -78,7 +87,9 @@ class App extends Component {
                 <User
                   {...props}
                   getUser={this.getUser}
+                  getUserRepos={this.getUserRepos}
                   user={user}
+                  repos={repos}
                   loading={loading}
                 />
               )}
