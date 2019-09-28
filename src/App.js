5c3f60cc5ba23714c859f4b839a68a5e3e936ce9
shubhamexpo <skd8238@gmail.com>
2019-09-28 14:39:43 +0530

    single user component and data

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Fragment, Component } from 'react'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import Navbar from './components/layout/Navbar'
 import Users from './components/users/Users'
+import User from './components/users/User'
 import Search from './components/users/Search'
 import Alert from './components/layout/Alert'
 import About from './components/pages/About'
@@ -12,6 +13,7 @@ import './App.css'
 class App extends Component {
   state = {
     users: [],
+    user: {},
     loading: false,
     alert: null
   }
@@ -30,6 +32,14 @@ class App extends Component {
     )
     this.setState({ users: res.data.items, loading: false })
   }
+  //Get Single Github User
+  getUser = async username => {
+    this.setState({ loading: true })
+    const res = await axios.get(
+      `https://api.github.com/users/${username}?&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+    )
+    this.setState({ user: res.data, loading: false })
+  }
 
   claerUsers = () => this.setState({ users: [], loading: false })
 
@@ -39,6 +49,7 @@ class App extends Component {
   }
 
   render() {
+    const { users, user, loading } = this.state
     return (
       <Router>
         <div className='App'>
@@ -55,14 +66,23 @@ class App extends Component {
                     showClear={this.state.users.length > 0 ? true : false}
                     setAlert={this.setAlert}
                   />
-                  <Users
-                    loading={this.state.loading}
-                    users={this.state.users}
-                  />
+                  <Users loading={loading} users={users} />
                 </Fragment>
               )}
             />
             <Route exact path='/about' component={About} />
+            <Route
+              exact
+              path='/user/:login'
+              render={props => (
+                <User
+                  {...props}
+                  getUser={this.getUser}
+                  user={user}
+                  loading={loading}
+                />
+              )}
+            />
           </Switch>
           <div className='container'>
             <Alert alert={this.state.alert} />
